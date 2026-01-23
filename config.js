e324af9cd11f3410d31d2f160767f9153d16fc9c
munnaff999 <ta398479@gmail.com>
2026-01-24 01:50:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // config.js
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAZ1fygNBz7wBMbMta10my7gtXg2rtF_4Y",
+  authDomain: "xdevilearningtelegram.firebaseapp.com",
+  projectId: "xdevilearningtelegram",
+  appId: "1:676347528676:web:ef137be8c1efabbb6e73f1"
 };
 
 firebase.initializeApp(firebaseConfig);
