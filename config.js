513dac3f4defcb6d1b77be919b0c45ace28a7e27
munnaff999 <ta398479@gmail.com>
2026-01-24 01:29:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
-const SUPABASE_URL = "https://sqgmfbjllqdbzyfozufy.supabase.co";
-const SUPABASE_KEY = "PASTE_YOUR_ANON_KEY_HERE"; // anon key only
+// config.js
 
-const ADMIN_TELEGRAM_ID = 123456789; // apna telegram numeric ID
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID"
+};
 
-const supabase = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
-
-// TEMP TELEGRAM ID (jab tak real TG connect na ho)
-const TELEGRAM_USER_ID = 111111;
-const TELEGRAM_USERNAME = "test_user";
+firebase.initializeApp(firebaseConfig);
