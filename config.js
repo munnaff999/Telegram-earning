1ca50d122bbba6aa479588ae74ea9b6492d4e42f
munnaff999 <ta398479@gmail.com>
2026-01-24 08:08:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAZ1fygNBz7wBMbMta10my7gtXg2rtF_4Y",
+    authDomain: "xdevilearningtelegram.firebaseapp.com",
+    projectId: "xdevilearningtelegram",
+    storageBucket: "xdevilearningtelegram.firebasestorage.app",
+    messagingSenderId: "676347528676"
+    appId: "YOUR_APP_ID: "1:676347528676:web:ef137be8c1efabbb6e73f1",
 };
 
 const app = initializeApp(firebaseConfig);
