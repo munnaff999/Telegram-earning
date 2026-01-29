760b6b0f73615b97dd4553a55270dfd7a475852c
munnaff999 <ta398479@gmail.com>
2026-01-29 21:32:02 +0530

    Update ads-monetag.js

--- js/ads-monetag.js
+++ js/ads-monetag.js
@@ -6,3 +6,37 @@ function watchAd(){
 function installAd(){
   completeOffer(25)
 }
+// INSTALL & EARN (Rewarded Popup)
+function showInstallOffer() {
+  show_10511608('pop')
+    .then(() => {
+      alert('Offer opened! Install & complete to earn.');
+      // yahan future me reward logic aayega
+    })
+    .catch(() => {
+      console.log('Monetag popup failed');
+    });
+}
+
+// UNLOCK REWARD (Rewarded Interstitial)
+function showRewardedAd() {
+  show_10511608()
+    .then(() => {
+      alert('Ad watched! Reward unlocked.');
+      // yahan coins / unlock ka code aayega
+    });
+}
+
+// IN-APP INTERSTITIAL (LIMITED AUTO)
+function showInAppAd() {
+  show_10511608({
+    type: 'inApp',
+    inAppSettings: {
+      frequency: 2,
+      capping: 0.1,
+      interval: 30,
+      timeout: 5,
+      everyPage: false
+    }
+  });
+}
