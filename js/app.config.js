3c7db150078a915394acbdbe02433c6e30132c6f
munnaff999 <ta398479@gmail.com>
2026-01-30 21:35:18 +0530

    Create app.config.js

---
+++ js/app.config.js
@@ -0,0 +1,7 @@
+// config/app.config.js
+
+export const SUPABASE_URL =
+  "https://sqgmfbjllqdbzyfozufy.supabase.co";
+
+export const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxZ21mYmpsbHFkYnp5Zm96dWZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMzc4MDgsImV4cCI6MjA4NDcxMzgwOH0.mLzb9BozHyXEG5un9zEfIHIOUhDf0x9iJq6xVh1lIQY";
