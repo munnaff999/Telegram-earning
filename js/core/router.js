56cd0c6ed40afae28568be3075b1df8ed8008938
munnaff999 <ta398479@gmail.com>
2026-01-31 14:13:53 +0530

    Create router.js

---
+++ js/core/router.js
@@ -0,0 +1,13 @@
+export function initRouter() {
+  loadPage("home");
+
+  window.addEventListener("hashchange", () => {
+    const page = location.hash.replace("#/", "") || "home";
+    loadPage(page);
+  });
+}
+
+async function loadPage(page) {
+  const res = await fetch(`/pages/${page}.html`);
+  document.getElementById("app").innerHTML = await res.text();
+}
