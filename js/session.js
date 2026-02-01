cf760c3e61eb8f31d9c5eadd6ffd519fa1e4739a
munnaff999 <ta398479@gmail.com>
2026-02-01 20:28:58 +0530

    Update session.js

--- js/session.js
+++ js/session.js
@@ -1,5 +1,21 @@
-// js/session.js
-const user = localStorage.getItem("user_email");
-if (!user) {
-  window.location.href = "login.html";
+function checkSessionAndRedirect() {
+  const user = localStorage.getItem("user");
+
+  if (user) {
+    window.location.href = "pages/home.html";
+  } else {
+    window.location.href = "login.html";
+  }
+}
+
+function requireLogin() {
+  const user = localStorage.getItem("user");
+  if (!user) {
+    window.location.href = "../login.html";
+  }
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "../login.html";
 }
