fbcb7bcf1646b8c2f4e6eb6e9cc2190a25980bd6
munnaff999 <ta398479@gmail.com>
2026-02-01 20:18:24 +0530

    Update balance.js

--- js/balance.js
+++ js/balance.js
@@ -5,3 +5,18 @@ function loadBalance() {
   if (el) el.innerText = "₹" + bal;
 }
 loadBalance();
+function loadBalance() {
+  let bal = localStorage.getItem("balance");
+  if (!bal) {
+    bal = 0;
+    localStorage.setItem("balance", 0);
+  }
+
+  const el = document.getElementById("user-balance");
+  if (!el) return;
+
+  el.classList.add("pulse");
+  el.innerText = "₹" + bal;
+
+  setTimeout(() => el.classList.remove("pulse"), 300);
+}
