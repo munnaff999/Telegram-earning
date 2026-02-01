aecca43a5b774b6cbd6b86520684ff7574276a30
munnaff999 <ta398479@gmail.com>
2026-02-01 19:34:43 +0530

    Delete js directory

--- js/core/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-export const supabase = createClient(
-  'https://YOUR_PROJECT_ID.supabase.co',
-  'YOUR_PUBLIC_ANON_KEY'
-);
