af87f7fe2d5c07202fbd41792717e68048c9b9c8
munnaff999 <ta398479@gmail.com>
2026-02-01 06:17:47 +0530

    Update router.js

--- js/core/router.js
+++ js/core/router.js
@@ -1,5 +1,13 @@
 export async function loadPage(page) {
   const res = await fetch(`pages/${page}.html`);
   const html = await res.text();
+
   document.getElementById('app').innerHTML = html;
+
+  try {
+    const module = await import(`../modules/${page}.js`);
+    if (module.init) module.init();
+  } catch (e) {
+    console.log(`No JS for ${page}`);
+  }
 }
