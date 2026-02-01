3f5bcda017041fd8442121a5de998949a5e4b6b0
munnaff999 <ta398479@gmail.com>
2026-02-01 06:08:15 +0530

    Delete js directory

--- js/core/guards.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from "./supabase.js";
-
-export async function requireAuth() {
-  const { data } = await supabase.auth.getUser();
-  if (!data.user) {
-    location.hash = "#/login";
-    throw "Not logged in";
-  }
-}
