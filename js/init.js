db3d973d708883c8c27ad93ee60b573b1ec0df9d
munnaff999 <ta398479@gmail.com>
2026-02-02 05:17:41 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-const SUPABASE_KEY = "PUBLIC_ANON_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxZ21mYmpsbHFkYnp5Zm96dWZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMzc4MDgsImV4cCI6MjA4NDcxMzgwOH0.mLzb9BozHyXEG5un9zEfIHIOUhDf0x9iJq6xVh1lIQY";
 
 const supabase = supabaseJs.createClient(
   SUPABASE_URL,
