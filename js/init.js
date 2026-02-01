5eb99d9ff658dc458a4905c239d2c4c93b26cdf2
munnaff999 <ta398479@gmail.com>
2026-02-01 19:50:56 +0530

    Delete js directory

--- js/init.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://YOUR.supabase.co";
-const SUPABASE_KEY = "PUBLIC_ANON_KEY";
-
-window.supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
