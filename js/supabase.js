c91d2cb78b1a7dd54d2c182b529acd19d9738619
munnaff999 <ta398479@gmail.com>
2026-02-02 19:16:00 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -14,7 +14,7 @@ const SUPABASE_CONFIG = {
   },
   PROD: {
     url: "https://sqgmfbjllqdbzyfozufy.supabase.co",
-    anon: "YOUR_PUBLIC_ANON_KEY"
+    anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxZ21mYmpsbHFkYnp5Zm96dWZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMzc4MDgsImV4cCI6MjA4NDcxMzgwOH0.mLzb9BozHyXEG5un9zEfIHIOUhDf0x9iJq6xVh1lIQY"
   }
 };
 
